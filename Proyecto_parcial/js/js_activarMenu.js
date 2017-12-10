cc0e29c452946acd8d78e44b926319fa43a921db
Daniel Torres <dstorres2011@hotmail.com>
2017-12-10 18:02:42 -0500

    ajustes Login Signed-off-by: Daniel Torres <dstorres2011@hotmail.com>

--- Proyecto_parcial/js/js_activarMenu.js
+++ Proyecto_parcial/js/js_activarMenu.js
@@ -1,11 +1,35 @@
+var conteo=0;
 $(document).ready(function() {
     $('#itemsHorario').click(function(){
-
-    	if (document.getElementById("itemsHorario").value ==  "1" ){
+    	if ( conteo>0 ){
     		return true;
     	}else{
     		return false;
     	}
 	});
 	
+	$('#itemsaula').click(function(){
+    	if ( conteo>0 ){
+    		return true;
+    	}else{
+    		return false;
+    	}
+	});
 });
+
+function getGET()
+{
+    var loc = document.location.href;
+    if(loc.indexOf('?')>0)
+    {
+        var getString = loc.split('?')[1];
+        var GET = getString.split('&');
+        var get = {};
+        for(var i = 0, l = GET.length; i < l; i++){
+            var tmp = GET[i].split('=');
+            get[tmp[0]] = unescape(decodeURI(tmp[1]));
+            conteo++;
+        }
+        return get;
+    }
+}
